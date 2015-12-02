a777404a96cf9bab2e44960d78457f83b5a4f99f
RyanStLouis <ryan.stlouis@ocsbstudent.ca>
2015-12-02 12:58:27 -0500

    car class

---
+++ CarClass/CarClass.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace CarClass
+{
+    partial class frmCarClass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnNewCar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(478, 292);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnNewCar
+            // 
+            this.btnNewCar.BackColor = System.Drawing.Color.Aqua;
+            this.btnNewCar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewCar.ForeColor = System.Drawing.Color.Black;
+            this.btnNewCar.Location = new System.Drawing.Point(158, 324);
+            this.btnNewCar.Name = "btnNewCar";
+            this.btnNewCar.Size = new System.Drawing.Size(203, 58);
+            this.btnNewCar.TabIndex = 1;
+            this.btnNewCar.Text = "New Car";
+            this.btnNewCar.UseVisualStyleBackColor = false;
+            this.btnNewCar.Click += new System.EventHandler(this.btnNewCar_Click);
+            // 
+            // frmCarClass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 408);
+            this.Controls.Add(this.btnNewCar);
+            this.Controls.Add(this.listBox1);
+            this.Name = "frmCarClass";
+            this.Text = "Car Class";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnNewCar;
+    }
+}
+
---
+++ CarClass/CarClass.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Created by: Ryan St. Louis
+ * Created on: 15/12/02
+ * Created for: ICS3U
+ * Daily Assignment – Unit 8-01
+ * This program does nothing
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarClass
+{
+    public partial class frmCarClass : Form
+    {
+        class Vehicle
+        {
+            private string _liscensePlate;
+            private string _colour;
+            private int _numOfDoors;
+            private int _speed;
+            private int _maxSpeed;
+
+            public void Accelerate(int speedIncrease)
+            {
+                this._speed = speedIncrease + this._speed;
+            }
+            public void Brake(int speedDecrease)
+            {
+                this._speed = this._speed - speedDecrease;
+            }
+            public string CurrentState()
+            {
+                string returnString;
+
+                returnString = "Speed: " + this._speed + "Liscense Plate: " + this._liscensePlate +
+                    "# of doors: " + this._numOfDoors + "Colour: " + this._colour;
+
+                return returnString;
+            }
+        }
+
+        public frmCarClass()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNewCar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
